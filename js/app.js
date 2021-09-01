7a0ab7a28495ffa6fb8de1f5eeb63ab3eaa70266
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-09-01 08:58:28 -0600

    added queryselectorall and looped trhough buttons to toggle class of none

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const buttons = document.querySelectorAll('button');
+
+for (const button of buttons) {
+    button.addEventListener('click', function() {
+        modal = document.querySelector('.modalbox');
+        modal.classList.toggle('none')
+})}
